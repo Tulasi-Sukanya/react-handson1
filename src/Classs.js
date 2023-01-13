72ea754c709e6b2a6cdc77b6a0ca3385dc23e330
Tulasi-Sukanya <sukanya271.tulasi@gmail.com>
2023-01-13 21:00:29 +0530

    changes done

--- src/Classs.js
+++ src/Classs.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import {Component} from 'react'
 import './App.css'
 
-const Classs = () => {
-  return (
-    <div className="cc">
-        <h1>This is Created Using Class Component</h1>
-        <h4>This is done using External Css.</h4>
-        <h5>This is done using Inline Css.</h5>
-    </div>
-  )
+class Classs extends Component{
+  render(){
+    return(
+      <div className="cc">
+      <h1>This is Created Using Class Component</h1>
+      <h4>This is done using External Css.</h4>
+      <h5>This is done using Inline Css.</h5>
+  </div>
+    )
+  }
 }
 
 export default Classs
