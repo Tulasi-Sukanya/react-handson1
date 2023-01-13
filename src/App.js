9d44c71d3d6e4f5de3cc081743c2422a7f0c31d5
Tulasi-Sukanya <sukanya271.tulasi@gmail.com>
2023-01-13 20:53:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import { useState } from "react";
+import  Function from './Function'
+import Classs from "./Classs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [show,setshow]=useState(false)
+  const [showclass,setshowclass]=useState(false)
+
+  const handleClick=()=>{
+    setshow(!show)
+  }
+  const handleClass=()=>{
+    setshowclass(!showclass)
+  }
+ 
+  return(
+    <div className="app">
+      <h1>Styling Using Function And Class Component</h1><br></br>
+      <button onClick={handleClick} className="btn1">To see styling in Functional Component</button>
+      <button  onClick={handleClass} className="btn2">To see styling in Class Component</button>
+      {show ? <Function /> : " "}
+      {showclass ? <Classs />:" "}
     </div>
   );
 }
---
+++ src/Classs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const Classs = () => {
+  return (
+    <div className="cc">
+        <h1>This is Created Using Class Component</h1>
+        <h4>This is done using External Css.</h4>
+        <h5>This is done using Inline Css.</h5>
+    </div>
+  )
+}
+
+export default Classs
